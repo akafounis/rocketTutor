60e9f48ac8688067340dee3efefe0e30d530a160
akafounis <kafounis.a@gmail.com>
2022-03-01 19:04:09 +0100

    create user table

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import Users from './Users'
+import theme from './theme';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    {/* <App /> */}
+    <Users />
+  </ThemeProvider>,
+  document.querySelector('#root'),
+);
